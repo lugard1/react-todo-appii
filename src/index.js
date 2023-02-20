80ab4794330dae8ac69675b6fa51765ae148950b
Lugard Agu <agulugard@gmail.com>
2023-02-21 00:37:23 +0100

    update files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // component file
-import TodoContainer from './TodoContainer';
+// eslint-disable-next-line import/extensions
+import TodoContainer from './components/TodoContainer';
 
 // stylesheet
 import './App.css';
